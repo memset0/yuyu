728b8e4885a2b14f431f8eb11f06acf1373f8efe
memset0 <memset0@outlook.com>
2020-08-04 21:55:24 +0800

    feat: 命令行调用编辑器编辑功能

---
+++ script/cmdtools.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+const router = require('./router');
+const config = require('./global').config;
+
+let cmd = {
+	edit: function (uri) {
+		if (!uri.startsWith('/')) uri = '/' + uri;
+		if (!uri.endsWith('/')) uri = uri + '/';
+
+		if (!Object.keys(router.routes).includes(uri)) {
+			console.log('No such uri!');
+			return;
+		}
+
+		let file = router.routes[uri];
+		exec(config.command.edit.replace(/%s/g, file.path));
+	}
+};
+
+module.exports = cmd;
